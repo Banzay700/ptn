d629a8ec8f500ff4b7c922f7db6075db005a96c2
Oleksandr Lotko <lotkoolexandr@gmail.com>
2023-10-01 20:43:59 +0300

    simple tests

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print(2 + 2)
+def get_full_name(first_name, second_name):
+    full_name = first_name + ' ' + second_name
+    return full_name
+
+
+full_name = get_full_name('Oleksandr', 'Lotko')
+test = full_name.upper()
+
+print(test)
+print(full_name)
